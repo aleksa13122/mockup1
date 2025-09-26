b7d9f619c81cf0ebf0dbe8c5f547c903a82a31ad
Aleksa Subotin <akacar9@gmail.com>
2025-09-26 14:08:13 +0200

    navigation icons change

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,8 @@
 import "./Navigation.css";
+import menuIcon from "../assets/images/icons/menu_book_2_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
+import homeIcon from "../assets/images/icons/home_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
+import instaIcon from "../assets/images/icons/icons8-instagram.svg";
+import fbIcon from "../assets/images/icons/icons8-facebook.svg";
 
 export default function Navigation() {
   return (
@@ -6,16 +10,25 @@ export default function Navigation() {
       <h1 className="nav-header">AURUM</h1>
       <div className="nav-bar">
         <div className="nav-btns1">
-          <button>HOME</button>
-          <button>MENU</button>
+          <button className="home-btn">
+            <img src={homeIcon} alt="Home" />
+          </button>
+          <button className="menu-btn">
+            <img src={menuIcon} alt="Menu" />
+            Menu
+          </button>
         </div>
         <div className="nav-btns2">
           <button>BOOK A TABLE</button>
           <button>ORDER ONLINE</button>
         </div>
         <div className="nav-btns3">
-          <button>INSTA</button>
-          <button>FB</button>
+          <button className="insta-btn">
+            <img src={instaIcon} alt="Insta" />
+          </button>
+          <button className="fb-btn">
+            <img src={fbIcon} alt="Fb" />
+          </button>
         </div>
       </div>
     </div>

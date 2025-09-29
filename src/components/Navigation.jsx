e51fe87ac3fed087df3a4922c05ae1fe2fdd6c58
Aleksa Subotin <akacar9@gmail.com>
2025-09-29 17:35:53 +0200

    navigation icons change

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,8 @@
 import "./Navigation.css";
 import menuIcon from "../assets/images/icons/menu_book_2_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
 import homeIcon from "../assets/images/icons/home_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
-import instaIcon from "../assets/images/icons/icons8-instagram.svg";
 import fbIcon from "../assets/images/icons/icons8-facebook.svg";
+import { ReactComponent as InstaIcon } from "../assets/images/icons/icons8-instagram.svg";
 
 export default function Navigation() {
   return (
@@ -24,10 +24,10 @@ export default function Navigation() {
         </div>
         <div className="nav-btns3">
           <button className="insta-btn">
-            <img src={instaIcon} alt="Insta" />
+            <InstaIcon alt="Insta" className="insta-icon" />
           </button>
           <button className="fb-btn">
-            <img src={fbIcon} alt="Fb" />
+            <img src={fbIcon} alt="Fb" className="fb-icon" />
           </button>
         </div>
       </div>

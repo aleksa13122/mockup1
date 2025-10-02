0a75a23cca94c6ea46839717306dfc5e7b4b0bc8
Aleksa Subotin <akacar9@gmail.com>
2025-10-02 17:15:22 +0200

    navigation done?

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,66 +2,20 @@ import "./Navigation.css";
 
 export default function Navigation() {
   return (
-    <div className="navigation">
+    <div className="nav-bar">
       <h1 className="nav-header">AURUM</h1>
-      <div className="nav-bar">
-        <div className="nav-btns1">
-          <button className="home-btn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#e3e3e3"
-              className="home-icon"
-            >
-              <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
-            </svg>
-          </button>
-          <button className="menu-btn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#e3e3e3"
-              className="menu-icon"
-            >
-              <path d="M240-80q-33 0-56.5-23.5T160-160v-80h-40v-80h40v-120h-40v-80h40v-120h-40v-80h40v-80q0-33 23.5-56.5T240-880h480q33 0 56.5 23.5T800-800v640q0 33-23.5 56.5T720-80H240Zm0-80h480v-640H240v80h40v80h-40v120h40v80h-40v120h40v80h-40v80Zm0 0v-640 640Zm140-120h60v-160q26-7 43-28.5t17-48.5v-163h-40v151h-30v-151h-40v151h-30v-151h-40v163q0 27 17 48.5t43 28.5v160Zm220 0h60v-400q-50 0-85 35t-35 85v120h60v160Z" />
-            </svg>
-            Menu
-          </button>
-        </div>
-        <div className="nav-btns2">
-          <button className="table-btn">BOOK A TABLE</button>
-          <button className="order-btn">ORDER ONLINE</button>
-        </div>
-        <div className="nav-btns3">
-          <button className="fb-btn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 30 30"
-              width="30px"
-              height="30px"
-              className="fb-icon"
-            >
-              <path d="M15,3C8.373,3,3,8.373,3,15c0,6.016,4.432,10.984,10.206,11.852V18.18h-2.969v-3.154h2.969v-2.099c0-3.475,1.693-5,4.581-5 c1.383,0,2.115,0.103,2.461,0.149v2.753h-1.97c-1.226,0-1.654,1.163-1.654,2.473v1.724h3.593L19.73,18.18h-3.106v8.697 C22.481,26.083,27,21.075,27,15C27,8.373,21.627,3,15,3z" />
-            </svg>
-          </button>
-          <button className="insta-btn">
-            <svg
-              fill="#000000"
-              width="32px"
-              height="32px"
-              viewBox="0 0 32 32"
-              xmlns="http://www.w3.org/2000/svg"
-              className="insta-icon"
-            >
-              <path d="M20.445 5h-8.891A6.559 6.559 0 0 0 5 11.554v8.891A6.559 6.559 0 0 0 11.554 27h8.891a6.56 6.56 0 0 0 6.554-6.555v-8.891A6.557 6.557 0 0 0 20.445 5zm4.342 15.445a4.343 4.343 0 0 1-4.342 4.342h-8.891a4.341 4.341 0 0 1-4.341-4.342v-8.891a4.34 4.34 0 0 1 4.341-4.341h8.891a4.342 4.342 0 0 1 4.341 4.341l.001 8.891z" />
-              <path d="M16 10.312c-3.138 0-5.688 2.551-5.688 5.688s2.551 5.688 5.688 5.688 5.688-2.551 5.688-5.688-2.55-5.688-5.688-5.688zm0 9.163a3.475 3.475 0 1 1-.001-6.95 3.475 3.475 0 0 1 .001 6.95zM21.7 8.991a1.363 1.363 0 1 1-1.364 1.364c0-.752.51-1.364 1.364-1.364z" />
-            </svg>
-          </button>
-        </div>
+      <div className="nav-btns">
+        <button className="table-btn">FOOD</button>
+        <button className="order-btn">DRINKS</button>
+        <button className="table-btn">BOOK A TABLE</button>
+        <button className="table-btn">BOOK A TABLE</button>
+        <button className="order-btn">CONTACT</button>
+      </div>
+      <div className="language-btns">
+        <button>US</button>
+        <button>EU</button>
+        <button>MT</button>
+        <button>GER</button>
       </div>
     </div>
   );

a9902d0d807c94a5e3218847b4859325d0944352
Aleksa Subotin <akacar9@gmail.com>
2025-10-02 23:00:46 +0200

    layour changes

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,5 +1,19 @@
 import "./Events.css";
 
 export default function Events() {
-  return <div className="events"></div>;
+  return (
+    <div className="events">
+      <div className="event-card">
+        <div className="event-picture"></div>
+        <div className="event-desc">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id,
+            aliquam! Porro maiores aliquid
+          </p>
+        </div>
+      </div>
+      <div className="event-card">EVENT 1</div>
+      <div className="event-card">EVENT 1</div>
+    </div>
+  );
 }

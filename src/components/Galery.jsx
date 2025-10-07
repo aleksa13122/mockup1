f15e4b4c16a0a0297570dfa25aa5217f9f5985d3
Aleksa Subotin <akacar9@gmail.com>
2025-10-07 15:13:51 +0200

    gallery done

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -19,16 +19,22 @@ export default function Galery() {
           Voluptatem labore a dolor consequuntur veritatis ullam veniam
           quibusdam cum aspernatur
         </p>
-        <button className="galery-btn">SEE MORE</button>
+        <button className="galery-btn">
+          <span>SEE MORE</span>
+        </button>
       </div>
       <div className="galery-description">
-        <h3>DRINKS</h3>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-        labore a dolor consequuntur veritatis ullam veniam quibusdam cum
-        aspernatur Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Voluptatem labore a dolor consequuntur veritatis ullam veniam quibusdam
-        cum aspernatur
-        <button>SEE MORE</button>
+        <h3 className="galery-heading heading-drinks">DRINKS</h3>
+        <p className="galery-text text-drinks">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          labore a dolor consequuntur veritatis ullam veniam quibusdam cum
+          aspernatur Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatem labore a dolor consequuntur veritatis ullam veniam
+          quibusdam cum aspernatur
+        </p>
+        <button className="galery-btn btn-drinks">
+          <span>SEE MORE</span>
+        </button>
       </div>
       <div className="galery-picture">
         <img src={img2} alt="drinks" className="drinks-img" />
@@ -37,7 +43,7 @@ export default function Galery() {
         <img src={img3} alt="food" />
       </div>
       <div className="galery-description">
-        <h3 className="galery-heading">AMBIENT</h3>
+        <h3 className="galery-heading">AMBIENCE</h3>
         <p className="galery-text">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
           labore a dolor consequuntur veritatis ullam veniam quibusdam cum
@@ -45,7 +51,9 @@ export default function Galery() {
           Voluptatem labore a dolor consequuntur veritatis ullam veniam
           quibusdam cum aspernatur
         </p>
-        <button className="galery-btn">SEE MORE</button>
+        <button className="galery-btn">
+          <span>SEE MORE</span>
+        </button>
       </div>
     </div>
   );

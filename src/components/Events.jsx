b9fdd0581b7d35262ff50447de06a151a314af3b
Aleksa Subotin <akacar9@gmail.com>
2025-10-08 11:33:43 +0200

    events changes

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,23 +1,23 @@
 import "./Events.css";
-import food3 from "../assets/images/food3.jpg";
-import food2 from "../assets/images/food2.jpg";
-import food1 from "../assets/images/food1.jpg";
+import img3 from "../assets/images/drinks1.jpg";
+import img2 from "../assets/images/food1.jpg";
+import img1 from "../assets/images/food4.jpg";
 
 export default function Events() {
   return (
     <div className="events">
-      <div className="event-card">
-        <img src={food1} alt="Food" className="card-img" />
-        <h3 className="card-title">Mediterranean Delight</h3>
-      </div>
-      <div className="event-card">
-        <img src={food2} alt="Food" className="card-img" />
-        <h3 className="card-title">Mediterranean Delight</h3>
-      </div>
-      <div className="event-card">
-        <img src={food3} alt="Food" className="card-img" />
-        <h3 className="card-title">Mediterranean Delight</h3>
-      </div>
+      <a className="event-card">
+        <img src={img1} alt="Food" className="card-img" />
+        <h3 className="card-title">BRUNCH</h3>
+      </a>
+      <a className="event-card">
+        <img src={img2} alt="Food" className="card-img" />
+        <h3 className="card-title">MEDITERANIAN DELIGHT</h3>
+      </a>
+      <a className="event-card">
+        <img src={img3} alt="Food" className="card-img" />
+        <h3 className="card-title">FRESH JUICES</h3>
+      </a>
     </div>
   );
 }

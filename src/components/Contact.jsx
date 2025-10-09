f6900bb8f0f933bf2a44d4f9008575aea4d59574
Aleksa Subotin <akacar9@gmail.com>
2025-10-09 09:14:26 +0200

    contact dv changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,32 @@ import "./Contact.css";
 export default function Contact() {
   return (
     <div className="contact">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, autem
-      odio architecto ab explicabo voluptate suscipit hic ducimus quis voluptas
-      dolorem eligendi iste, perspiciatis adipisci rerum laborum ad consequuntur
-      accusamus?
+      <h2 className="contact-header">AURUM</h2>
+      <div className="contact-grid">
+        <div>
+          <h3>WORKING HOURS</h3>
+          <li>MONDAY-THURSDAY 9-24h</li>
+          <li>WEEKENDS 9-02h</li>
+        </div>
+        <div>
+          <h3>CONTACT</h3>
+          <li>PHONE NUM</li>
+          <li>INSTAGRAM FACEBOOK</li>
+          <li></li>
+        </div>
+        <div>
+          <h3>WORKING HOURS</h3>
+          <li>
+            <a>MONDAY-FRIDAY 24/7</a>
+          </li>
+          <li>
+            <a>MONDAY-FRIDAY 24/7</a>
+          </li>
+          <li>
+            <a>MONDAY-FRIDAY 24/7</a>
+          </li>
+        </div>
+      </div>
     </div>
   );
 }

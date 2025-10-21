4fd8b486ae4f0c8251dba1ccfddd2e4ac2d2e223
Aleksa Subotin <akacar9@gmail.com>
2025-10-21 15:32:16 +0200

    pages done almost

--- src/components/pages/Food.jsx
+++ src/components/pages/Food.jsx
@@ -1,14 +1,30 @@
 import Navigation from "../Navigation";
 import Contact from "../Contact";
 import "../.././App.css";
+import menuImage from "../../assets/images/menu-picture.png";
 
 export default function Menu() {
   return (
     <>
       <div className="main-container">
         <Navigation />
-        <h1>Our Menu</h1>
-        <p>Explore Aurum’s refined culinary creations.</p>
+        <div className="first-column">
+          <h1 className="pages-header">Menu</h1>
+          <p className="pages-desc">
+            At Aurum, every dish is a masterpiece crafted with precision,
+            passion, and artistry. Our chefs combine the finest seasonal
+            ingredients with innovative techniques to create a symphony of
+            flavors that delight the senses. Each plate is more than a meal — it
+            is a curated experience designed to evoke emotion, capture beauty,
+            and celebrate the essence of haute cuisine.
+          </p>
+        </div>
+        <div className="image-container">
+          <img src={menuImage} alt="Restaurant menu" className="pages-image" />
+        </div>
+        <button className="pages-btn">
+          <span>CHECK OUT OUR MENU</span>
+        </button>
       </div>
       <div className="parallax">
         <a href="/reservations" className="parallax-btn">
